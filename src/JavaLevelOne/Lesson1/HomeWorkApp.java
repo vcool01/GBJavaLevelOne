e01178c9ae8b09c5716baec1548e37dcf7ea813c
vcool01 <v.cool-01@list.ru>
2021-09-23 16:32:48 +0300

    дз 1 правки

--- src/JavaLevelOne/Lesson1/HomeWorkApp.java
+++ src/JavaLevelOne/Lesson1/HomeWorkApp.java
@@ -43,7 +43,7 @@ public class HomeWorkApp {
 
         if (value <=0 ){
             System.out.println("Красный");
-        }else if (value <=100){
+        }else if (value > 0 && value <=100){
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
